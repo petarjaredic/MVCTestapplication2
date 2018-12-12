6a44eceb81240f0fcb08df71ad46e406d9d6bb27
Petar Jaredic <petar.jaredic@deblokt.com>
2018-12-12 12:58:38 +0100

    Initial commit

---
+++ TestApplication/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplication.Controllers
+{
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(string fname)
+        {
+            EchoClient.Client client = new EchoClient.Client();
+            var response = client.CreateUser(new EchoClient.Models.User() { UserName = fname });
+            if(response)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+    }
+}
